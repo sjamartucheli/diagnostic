3c608025cc0a67bdcc0f448f1504ec817a133fa0
Samuel Martucheli <sjamartucheli@gmail.com>
2015-03-18 15:51:38 -0300

    adding grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,6 +132,9 @@ module.exports = function(grunt) {
   //Test task.
   grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 
+  //Build task.
+  grunt.registerTask('build', ['uglify', 'cssmin']);
+
   // For Heroku users only.
   // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
   grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
